83ad7f2c1d045c335251eb48dd41c179cbf39516
Thiha2020 <thihaaung.wp@gmail.com>
2020-11-19 16:37:21 +0630

    Adding javascript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+                /*Overlay section*/
+                /* Open when someone clicks on the img icon element */
+                function openNav() {
+                    document.getElementById("myNav").style.width = "100%";
+                }
+                /* Close when someone clicks on the "x" symbol inside the overlay */
+                function closeNav() {
+                    document.getElementById("myNav").style.width = "0%";
+                }
